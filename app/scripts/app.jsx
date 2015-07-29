589e6280346880a0c33408f7f78f4654113ba082
Alana Cable <alanacable@gmail.com>
2015-07-28 22:43:13 -0500

    Attempt at trying both isomorphic and non-isomorphic in one example

--- app/scripts/app.jsx
+++ app/scripts/app.jsx
@@ -3,12 +3,16 @@ var React = require('react');
 var App = React.createClass({
 
   testClientScript: function() {
+  	console.log("****HERE")
     alert('component has mounted on client side!');
   },
 
   render: function() {
     return (
-      <button onClick={this.testClientScript}>Test Client Script</button>
+      <div>
+      	<h2>Hello Bancvue</h2>
+      	<button onClick={this.testClientScript}>Test Client Script</button>
+      </div>
     );
   }
 });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('node-jsx').install({ extension: '.jsx' });
 var express = require('express'),
     path = require('path'),
     React = require('react');
+var Router = require('react-router');    
 
 var app = express();
 
@@ -11,8 +12,21 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 var Html = React.createFactory(require('./app/index.jsx'));
 
 app.get('/', function(req,res) {
-  res.send('<!DOCTYPE HTML>' + React.renderToStaticMarkup(Html()));
+  var agent = req.headers['user-agent'];
+  console.log("agent", agent);
+  if (agent.search("Chrome") > 0) {
+  	console.log("I am traditional");
+  	res.sendFile(path.resolve('index.html'));
+  }
+  else {
+  	console.log("I am isomorphic");
+  	res.send('<!DOCTYPE HTML>' + React.renderToStaticMarkup(Html()));
+  }
+  
 });
+
+
+
 
 app.listen(1337, function() {
   console.log('App running at http://localhost:1337');
